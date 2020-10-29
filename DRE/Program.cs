618e2baf597f7d0f5abe7adfdcf3b587ab215c5e
СерЛен <@DESKTOP-0P0DPBQ>
2020-10-29 21:01:39 +0500

    Каво

---
+++ DRE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DRE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
